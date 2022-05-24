3af21d69b3ff6eebebc8f50407843e3c1a34585b
Violeta <LIS17110640@irapuato.tecnm.mx>
2022-05-23 19:14:34 -0500

    feat: añade useEffect para manejar el estado de loading, cuando se cambia el estado de este

--- src/UseState.js
+++ src/UseState.js
@@ -1,7 +1,20 @@
 import React from 'react';
+//Array vacio, unicamente la primera vez que se renderize
 
 function UseState({name}){
     const [error, setError] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
+    
+    React.useEffect(()=>{
+        console.log('inicio efecto')
+        if(loading){
+            setTimeout(()=>{
+                setLoading(false);
+            }, 2000)
+        }
+        console.log('fin efecto')
+    }, [loading])
+
     return (
         <div>
             <h2>
@@ -11,9 +24,13 @@ function UseState({name}){
             {error && (
                 <p>Error: El código de seguridad es incorrecto</p>
             )}
+            {loading && (
+                <p>Cargando... </p>
+            )}
             <input placeholder='Código de seguridad'/>
             <button onClick={()=>{
-                setError(!error)
+                //setError(!error)
+                setLoading(!loading)
             }}>Comprobar</button>
         </div>
     )
